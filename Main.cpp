0e75d07cbb733969e49b95582a11843997460632
rbabik <74736955+rbabik@users.noreply.github.com>
2020-12-04 00:06:15 -0500

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,19 @@
+/*
+FINAL
+main.cpp
+Created by Ryder Babik
+11/11/20
+Engineering Computation 24780-B
+main file for running the program
+*/
+
+
+#include "fssimplewindow.h"
+#include "ViewManager2.h"
+
+int main(void) {
+	
+	FsOpenWindow(16, 16, 1200, 700, 1, "MAZE MANIA - The Maze Runners");
+	ViewManager theManager;
+	theManager.manage();
+}
