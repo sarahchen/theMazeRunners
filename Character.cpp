145406d5e52ea4699665ac1cfd1a8513772ef749
rbabik <74736955+rbabik@users.noreply.github.com>
2020-12-10 20:48:50 -0500

    Update Character.cpp

--- Character.cpp
+++ Character.cpp
@@ -52,13 +52,15 @@ Character::Character(int xpos, int ypos, carType car, GLuint Id)
 	thisCar = car;
 	textID = Id;
 	gameOver = false;
-	//livesRemaining = STARTLIVES;
+	//livesRemaining = STARTLIVES;  //future expansion
 	score = 0;
 
+	//set dynamics
 	Force = 0;
 	accel = 0;
 	mu = 0.4;
 	
+	//set initial parameters and configuration
 	headingAngle = M_PI_2;
 	initPos.x = xpos;
 	initPos.y = ypos;
@@ -220,6 +222,7 @@ bool Character::isValidMove()
 	if (willHitObstacleFront && velocity > 0) { return false; }
 	if (willHitObstacleBack && velocity < 0) { return false; }
 
+	// if makes it through all that then this is a valid move!
 	else { return true; }
 }
 
@@ -228,6 +231,7 @@ bool Character::isValidOrientation()
 	calcEdges();
 	int myCornersX[] = { ax,dx, bx,cx };
 	int myCornersY[] = { ay,dy, by,cy };
+	
 	//first check limits of screen
 	if ((*std::min_element(myCornersX, myCornersX + 2) <= width / 8) || (*std::min_element(myCornersX + 2, myCornersX + 4) <= width / 8))
 	{
@@ -246,19 +250,22 @@ bool Character::isValidOrientation()
 		return false;
 	}
 
-
+	//check also to see if hit a wall
 	if (willHitObstacleFront) { return false; }
 	if (willHitObstacleBack) { return false; }
 
+	// if makes it through all that then this is a valid move!
 	else { return true; }
 }
 
+//future function to expand into multiple lives per game
 bool Character::isLastLife()
 {
 	if (livesRemaining > 1) { return false; }
 	else { return true; }
 }
 
+	//future expansion!
 void Character::decrementLives()
 {
 	if (!isLastLife()) {
@@ -270,6 +277,7 @@ void Character::decrementLives()
 void Character::calcEdges()
 {
 	//a is front left, b is back left, c is back right, d is front right
+	//calculated using geometry
 	ax = posCenter.x + thisCharacter.hei / 2 * cos(headingAngle) - thisCharacter.wid / 2 * sin(headingAngle);
 	ay = posCenter.y - thisCharacter.hei / 2 * sin(headingAngle) - thisCharacter.wid / 2 * cos(headingAngle);
 	bx = ax - thisCharacter.hei * cos(headingAngle);
@@ -292,7 +300,8 @@ void Character::draw()
 	glBindTexture(GL_TEXTURE_2D, textID);
 
 	glBegin(GL_QUADS);
-
+	
+	//draw car based on which one this character is
 	if (thisCar == regCar) {
 		glTexCoord2d(0.0, 0.0);
 		glVertex2i(ax, ay);
@@ -319,187 +328,4 @@ void Character::draw()
 	glDisable(GL_BLEND);
 	glDisable(GL_TEXTURE_2D);
 
-	//glColor3ub(0, 255, 0);
-	//glBegin(GL_QUADS);
-	//glVertex2d(ax, ay);
-	//glVertex2d(bx, by);
-	//glVertex2d(cx, cy);
-	//glVertex2d(dx, dy);
-	//glEnd();
 }
-//	initPos.y = ypos;
-//	thisCharacter.hei = 60;
-//	thisCharacter.wid = 30;
-//	thisCharacter.dep = 0;
-//
-//	FsGetWindowSize(width, height);
-//
-//	initialize();
-//}
-
-//void Character::initialize()
-//{
-//	energyLevel = FULLENERGY;
-//	setX(initPos.x);
-//	setY(initPos.y);
-//	setZ(initPos.z);
-//	setVel(0);
-//}
-//
-//void Character::reset()
-//{
-//	initialize();
-//}
-//
-//void Character::updateKinematics(double deltaT)
-//{
-//	cout << "Force: " << Force << "    Acceleration: " << accel << "    Velocity:  "  << velocity << endl;
-//
-//	if (isValidMove()) {
-//		posCenter.x += velocity * cos(headingAngle) * deltaT;
-//		posCenter.y -= velocity * sin(headingAngle) * deltaT;
-//		velocity += accel * deltaT;
-//		//if (velocity > 10) {
-//		//	velocity = 10;
-//		//	Force = 0;
-//		//}
-//		//if (velocity < -10) {
-//		//	velocity = -10;
-//		//	Force = 0;
-//		//}
-//		if (velocity > .5) {
-//			accel = -mu * GRAVITY + (Force / mass); 
-//		}
-//		else if (velocity < -0.5) { 
-//			accel = mu * GRAVITY + (Force / mass); 
-//		}   //change direction of friction!!
-//		else if (abs(velocity) <= 0.5) { 
-//			accel = (Force / mass); 
-//		}
-//
-//	//	if (abs(accel) < .01) { accel = 0; }
-//	}
-//
-//	else {
-//		velocity = 0;
-//		accel = 0;
-//		Force = 0;
-//	}
-//}
-//
-//void Character::adjustOrientation(double angleAdjust)
-//{
-//	headingAngle += angleAdjust;
-//	if (!(isValidOrientation())) { headingAngle -= angleAdjust; }
-//}
-//
-//bool Character::isValidMove()
-//{
-//	int myCornersX[] = { ax,dx, bx,cx };
-//	int myCornersY[] = { ay,dy, by,cy };
-//	//first check limits of screen
-//	if ((*std::min_element(myCornersX, myCornersX + 2) <= width/6 && velocity > 0) || (*std::min_element(myCornersX + 2, myCornersX + 4) <= width/6 && velocity < 0))
-//	{
-//		return false;}
-//	if ((*std::min_element(myCornersY, myCornersY + 2) <= height/5 && velocity > 0) || (*std::min_element(myCornersY + 2, myCornersY + 4) <= height/5 && velocity < 0))
-//	{return false;}
-//
-//	if ((*std::max_element(myCornersX, myCornersX + 2) >= width && velocity > 0) || (*std::max_element(myCornersX + 2, myCornersX + 4) >= width && velocity < 0))
-//	{
-//		return false;
-//	}
-//	if ((*std::max_element(myCornersY, myCornersY + 2) >= height && velocity > 0) || (*std::max_element(myCornersY + 2, myCornersY + 4) >= height && velocity < 0))
-//	{
-//		return false;
-//	}
-//
-//	else { return true; }
-//}
-//
-//bool Character::isValidOrientation()
-//{
-//	calcEdges();
-//	int myCornersX[] = { ax,dx, bx,cx };
-//	int myCornersY[] = { ay,dy, by,cy };
-//	//first check limits of screen
-//	if ((*std::min_element(myCornersX, myCornersX + 2) <= width / 6) || (*std::min_element(myCornersX + 2, myCornersX + 4) <= width / 6))
-//	{
-//		return false;
-//	}
-//	if ((*std::min_element(myCornersY, myCornersY + 2) <= height / 5) || (*std::min_element(myCornersY + 2, myCornersY + 4) <= height / 5))
-//	{
-//		return false;
-//	}
-//	if ((*std::max_element(myCornersX, myCornersX + 2) >= width) || (*std::max_element(myCornersX + 2, myCornersX + 4) >= width))
-//	{
-//		return false;
-//	}
-//	if ((*std::max_element(myCornersY, myCornersY + 2) >= height) || (*std::max_element(myCornersY + 2, myCornersY + 4) >= height))
-//	{
-//		return false;
-//	}
-//
-//	else { return true; }
-//}
-//
-//bool Character::isLastLife()
-//{
-//	if (livesRemaining > 1) { return false; }
-//	else { return true; }
-//}
-//
-//void Character::decrementLives()
-//{
-//	if (!isLastLife()) {
-//		livesRemaining -= 1;
-//	}
-//	else { gameOver = true; }
-//}
-//
-//void Character::calcEdges()
-//{
-//	//a is front left, b is back left, c is back right, d is front right
-//	ax = posCenter.x + thisCharacter.wid / 2 * cos(headingAngle) - thisCharacter.wid / 2 * sin(headingAngle);
-//	ay = posCenter.y - thisCharacter.wid / 2 * sin(headingAngle) - thisCharacter.hei / 2 * cos(headingAngle);;
-//	bx = ax - thisCharacter.hei * cos(headingAngle);
-//	by = ay + thisCharacter.hei * sin(headingAngle);
-//	cx = bx + thisCharacter.wid * sin(headingAngle);
-//	cy = by + thisCharacter.wid * cos(headingAngle);
-//	dx = cx + thisCharacter.hei * cos(headingAngle);
-//	dy = cy - thisCharacter.hei * sin(headingAngle);
-//}
-//
-//void Character::draw(ViewManager& theManager)
-//{
-//	//a is front left, b is back left, c is back right, d is front right
-//	//ax = posCenter.x + thisCharacter.wid / 2 * cos(headingAngle) - thisCharacter.wid / 2 * sin(headingAngle);
-//	//ay = posCenter.y - thisCharacter.wid / 2 * sin(headingAngle) - thisCharacter.hei / 2 * cos(headingAngle);;
-//	//bx = ax - thisCharacter.hei * cos(headingAngle);
-//	//by = ay + thisCharacter.hei * sin(headingAngle);
-//	//cx = bx + thisCharacter.wid * sin(headingAngle);
-//	//cy = by + thisCharacter.wid * cos(headingAngle);
-//	//dx = cx + thisCharacter.hei * cos(headingAngle);
-//	//dy = cy - thisCharacter.hei * sin(headingAngle);
-//	calcEdges();
-//	
-//	glEnable(GL_TEXTURE_2D);
-//	glEnable(GL_BLEND);
-//	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-//	glColor4d(1.0, 1.0, 1.0, 1.0);
-//	glBindTexture(GL_TEXTURE_2D, textID);
-//
-//	glBegin(GL_QUADS);
-//
-//	glTexCoord2d(1.0, 0.0);
-//	glVertex2i(ax, ay);
-//	glTexCoord2d(0.0, 0.0);
-//	glVertex2i(bx, by);
-//	glTexCoord2d(0.0, 1.0);
-//	glVertex2i(cx, cy);
-//	glTexCoord2d(1.0, 1.0);
-//	glVertex2i(dx, dy);
-//	glEnd();
-//
-//	glDisable(GL_BLEND);
-//	glDisable(GL_TEXTURE_2D);
-//}
