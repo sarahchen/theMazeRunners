8e5b5f73923180ab85e71ca0986c56f5506d0629
rbabik <74736955+rbabik@users.noreply.github.com>
2020-12-10 19:11:48 -0500

    Update Character.h

--- Character.h
+++ Character.h
@@ -67,11 +67,11 @@ public:
 	Character();
 
 	Character(int xpos, int ypos, carType car, GLuint Id);
-
+	
+	//set initial car values and parameters
 	void setCarInfo(int xpos, int ypos, carType car, GLuint Id);
 	void vehicleParams();
 	void assignId(carType car, GLuint Id);
-
 	void assignId(GLuint Id);
 
 	// get and set for all member variables
@@ -110,6 +110,7 @@ public:
 	void setHealth(int newHealth) { health = newHealth; }
 	void addScore(int amount) { score += amount; }
 	
+	//set functions based on user inputs in ViewManager
 	void setForce(double force) { Force = force; }
 	void adjustOrientation(double angleAdjust); 
 	void adjustForce(int forceAdjust) { Force += forceAdjust; }
@@ -124,6 +125,7 @@ public:
 	bool isValidOrientation();
 	void checkMove();
 	
+	//for checking if making legal move (won't hit obstacle)
 	void setObstacleFront(bool val) { willHitObstacleFront = val; }
 	void setObstacleBack(bool val) { willHitObstacleBack = val; }
 
@@ -134,6 +136,7 @@ public:
 	bool isLastLife();
 	void decrementLives();
 
+	//for if need to reinitialize the character
 	void initialize();
 	void reset();
 
@@ -144,49 +147,3 @@ public:
 	void draw();
 };
 
-//public:
-//	//class constructor
-//	Character() {};
-//	Character(int xpos, int ypos, GLuint Id);
-//
-//	// get and set for all member variables
-//	string getLabel() { return thisCharacter.label; }
-//	double getX() { return posCenter.x; }
-//	double getY() { return posCenter.y; }
-//	double getZ() { return posCenter.z; }
-//	double getVel() { return velocity; }
-//	double getHei() { return thisCharacter.hei; }
-//	double getWid() { return thisCharacter.wid; }
-//	double getDep() { return thisCharacter.dep; }
-//	bool characterIsAlive() { return !gameOver; }
-//
-//	void setLabel(const std::string& newLabel) { thisCharacter.label = newLabel; }
-//	void setX(double xCoord) { posCenter.x = xCoord; }
-//	void setY(double yCoord) { posCenter.y = yCoord; }
-//	void setZ(double zCoord) { posCenter.z = zCoord; }
-//	void setVel(double vel) { velocity = vel; }
-//	void setHei(double height) { thisCharacter.hei = height; }
-//	void setWid(double width) { thisCharacter.wid = width; }
-//	void setDep(double depth) { thisCharacter.dep = depth; }
-//	
-//	void setForce(double force) { Force = force; }
-//	void adjustOrientation(double angleAdjust); 
-//	void adjustForce(int forceAdjust) { Force += forceAdjust; }
-//	void adjustAcceleration(double acceleration) { accel += acceleration; }
-//
-//	bool isValidMove();
-//	bool isValidOrientation();
-//	void checkMove();
-//
-//	void calcEdges();
-//
-//	bool isLastLife();
-//	void decrementLives();
-//
-//	void initialize();
-//	void reset();
-//	void updateKinematics(double deltaT);
-//
-//	//draw character in new location and orientation
-//	void draw(ViewManager& theManager);
-//};
